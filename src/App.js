4eae352ab4fa665ca6257da29c05b77e9d32d7e3
lbaker15 <laelbaker@hotmail.co.uk>
2021-02-15 13:59:40 +0000

    vfs

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
           window.FB.AppEvents.logPageView();
           window.FB.getLoginStatus(function(response) {
             this.statusChangeCallback(response)
-            console.log(statusChangeCallback. window.statusChangeCallback, window.this)
+            console.log(statusChangeCallback, window.statusChangeCallback, window.this)
           });
           // window.fbAsyncInit()
  
